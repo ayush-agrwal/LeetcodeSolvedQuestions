31c8c5747668849c28f0e2327f6ea6c77aa292cf
Ayush Agrawal <145705235+ayush-agrwal@users.noreply.github.com>
2026-06-29 23:55:36 +0530

    Time: 14 ms (93.05%), Space: 51.5 MB (55.97%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,19 @@
+class Solution {
+    // Greedy Approach is the best for this problem
+    public int findContentChildren(int[] g, int[] s) {
+        Arrays.sort(g);
+        Arrays.sort(s);
+        int max = 0;
+        int i = 0, j = 0;
+        while(i < g.length  && j < s.length){
+            if(g[i] <= s[j]){
+                i += 1;
+                j += 1;
+                max += 1;
+            }else{
+                j += 1;
+            }
+        }
+        return max;
+    }
+}
