d1c80c87f28de356953cecb3d3099b80961f8682
Ayush Agrawal <145705235+ayush-agrwal@users.noreply.github.com>
2026-07-02 00:19:04 +0530

    Time: 546 ms (65.63%), Space: 0B (100%) - LeetHub

---
+++ 3586-find-covid-recovery-patients/3586-find-covid-recovery-patients.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+SELECT p.patient_id,
+       p.patient_name,
+       p.age,
+       DATEDIFF(MIN(t2.test_date), MIN(t1.test_date)) AS recovery_time
+FROM covid_tests t1
+INNER JOIN covid_tests t2 ON t1.patient_id = t2.patient_id AND t1.test_date < t2.test_date AND t1.result = 'Positive' AND t2.result = 'Negative'
+INNER JOIN patients p ON t1.patient_id = p.patient_id
+GROUP BY 1
+ORDER BY 4, 2;
